cf473438a164cc58f53f6c1562762d2b1cd695de
iFall-iFail <64719808+iFall-iFail@users.noreply.github.com>
2020-11-22 05:32:20 +0600

    A Casual Game to Play in Free Time
    Made With Unity Game Engine

---
+++ Bachao - Touch Me NOT/Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+
+public class EnemyBehavior : MonoBehaviour
+{
+    public float speed;
+    private float zBound = 14.5f;
+    private Rigidbody enemyRb;
+    private GameObject player;
+
+    public int scoreToAdd = 1;
+    private GameManager gameManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+        player = GameObject.Find("Player");
+
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+
+    }
+        
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameManager.isGameOn)
+        {
+            EnemyControl();
+        }
+    }
+
+    public void EnemyControl()
+    {
+        if (!gameManager.isGameOver)
+        {
+            Vector3 lookDirection = (player.transform.position - transform.position).normalized;
+            enemyRb.AddForce(lookDirection * speed);
+        }
+          
+
+        if (transform.position.z <= -zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zBound);
+        }
+
+        else if (transform.position.z >= zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zBound);
+        }
+
+    }
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Electric Bar"))
+        {
+            Destroy(gameObject);
+
+            gameManager.UpdateScore(scoreToAdd);
+
+        }
+    }
+
+}
